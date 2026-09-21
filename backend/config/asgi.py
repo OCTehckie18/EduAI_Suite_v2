4e149efd480807cbf9b51805212d31607bbfdd88
OCTehckie18 <omkar.chakraborty2001@gmail.com>
2026-09-22 00:03:16 +0530

    fix: remove duplicate import of websocket_urlpatterns in asgi.py

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,3 +1,4 @@
+from apps.quizzes.routing import websocket_urlpatterns
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -5,7 +6,6 @@ from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 django_asgi_app = get_asgi_application()
-from apps.quizzes.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
